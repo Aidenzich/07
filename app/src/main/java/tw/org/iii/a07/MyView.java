85b35af8cfcb31dcd1dec248edb29721f0570965
Aidenzich <aidenzich0221@gmail.com>
2019-11-15 11:36:37 +0800

    ddd

--- app/src/main/java/tw/org/iii/a07/MyView.java
+++ app/src/main/java/tw/org/iii/a07/MyView.java
@@ -12,13 +12,14 @@ import java.util.LinkedList;
 
 public class MyView extends View {
     //private LinkedList<Point> line; 一條線
-    private LinkedList<LinkedList<Point>> lines;  //多條線
+    private LinkedList<LinkedList<Point>> lines,recycler;  //多條線 //新建一個recycler作備份
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         setBackgroundColor(Color.BLUE);
 
         //line = new LinkedList<>();
         lines = new LinkedList<>();
+        recycler = new LinkedList<>();
     }
 
     @Override
@@ -26,6 +27,7 @@ public class MyView extends View {
         float ex = event.getX(), ey = event.getY();
         Point point = new Point(ex, ey);
         if (event.getAction() == MotionEvent.ACTION_DOWN){
+            recycler.clear();
             LinkedList<Point> line = new LinkedList<>();
             lines.add(line);
         }  //多條線時使用
@@ -61,8 +63,16 @@ public class MyView extends View {
         invalidate();//清掉
     }
     public void undo(){
-        lines.removeLast(); //移掉最後一條
-        invalidate();
+        if (lines.size()>0) {
+            lines.removeLast(); //移掉最後一條
+            invalidate();
+        }
+    }
+    public void redo(){
+        if (recycler.size()>0) {
+            lines.add(recycler.removeLast()); //移掉最後一條
+            invalidate();
+        }
     }
     private class Point {
         float x, y;
@@ -70,5 +80,4 @@ public class MyView extends View {
             this.x = x; this.y = y;
         }
     }
-
 }
